003dc17d849386fa4770e83e8f0d87eaf1f0678c
Fernando Moro <Morofernando99@gmail.com>
2026-03-06 21:58:02 -0300

    logging

--- src/data_utils.py
+++ src/data_utils.py
@@ -4,7 +4,6 @@ from aeon.datasets import load_classification
 def znorm(x): return (x - np.mean(x)) / np.std(x)
 
 def load_and_normalize_dataset(dataset_name):
-    print(f'Loading dataset {dataset_name}...')
     X_train, y_train = load_classification(dataset_name, split='train')
     X_test, y_test = load_classification(dataset_name, split='test')
     
